c2dc1baef800c08a9c6ea08a0b68c4c6551070ae
Alex Mark <alexvmar@yandex.ru>
2023-03-20 20:20:51 +0300

    change file end task

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string[] array = { "hello", "2", "world", ":-)" };
+string[] array2 = { "1234", "1567", "-2", "computer science" };
+string[] array3 = { "Russia", "Denmark", "Kazan" };
+//Для создания массива вывода результатов вычисляем его размер
+int size = 0;
+foreach (string item in array) if (item.Length <= 3) size++;
+
+Console.WriteLine($"Размер массива равен {size}");
+//Создаём массив определённой размерности
+string[] result = new string[size];
+
+int index = 0;
+foreach (string item in array)
+{
+    if (item.Length <= 3)
+    {
+        result[index] = item;
+        index++;
+    }
+}
+Console.WriteLine($" -> [{string.Join(", ", result)}]");
