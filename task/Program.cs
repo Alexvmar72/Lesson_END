f8a688275b46c4f34f8e8e3aac1455af678e8eea
Alex Mark <alexvmar@yandex.ru>
2023-03-20 20:44:06 +0300

    change 2 file task

--- task/Program.cs
+++ task/Program.cs
@@ -1,22 +1,44 @@
-﻿Console.Clear();
-string[] array = { "hello", "2", "world", ":-)" };
+﻿using System;
+using static System.Console;
+Clear();
+
+string[] GetArray(string[] array)
+{
+    //Для создания массива вывода результатов вычисляем его размер
+    int size = 0;
+    foreach (string item in array) if (item.Length <= 3) size++;
+
+    //Создаём массив определённой размерности
+    string[] result = new string[size];
+
+    int index = 0;
+    foreach (string item in array)
+    {
+        if (item.Length <= 3)
+        {
+            result[index] = item;
+            index++;
+        }
+    }
+
+    return result;
+}
+
+
+void PrintArray(string[] array)
+{
+    WriteLine($" -> [{string.Join(", ", array)}]");
+}
+
+
+string[] array1 = { "hello", "2", "world", ":-)" };
 string[] array2 = { "1234", "1567", "-2", "computer science" };
 string[] array3 = { "Russia", "Denmark", "Kazan" };
-//Для создания массива вывода результатов вычисляем его размер
-int size = 0;
-foreach (string item in array) if (item.Length <= 3) size++;
+string[] printarray = GetArray(array1);
 
-Console.WriteLine($"Размер массива равен {size}");
-//Создаём массив определённой размерности
-string[] result = new string[size];
 
-int index = 0;
-foreach (string item in array)
-{
-    if (item.Length <= 3)
-    {
-        result[index] = item;
-        index++;
-    }
-}
-Console.WriteLine($" -> [{string.Join(", ", result)}]");
+PrintArray(GetArray(array1));
+WriteLine();
+PrintArray(GetArray(array2));
+WriteLine();
+PrintArray(GetArray(array3));
